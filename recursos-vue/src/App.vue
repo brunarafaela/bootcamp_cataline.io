609eb401352e65a176dc4dded8291899b11a29b6
brunarafaela <brunarafaelav@outlook.com>
2021-09-29 23:19:04 -0300

    Aula diretivas

--- recursos-vue/src/App.vue
+++ recursos-vue/src/App.vue
@@ -1,30 +1,70 @@
 <template>
-    <h1>{{count}}</h1>
-    <button @click="count++">add</button>
-    <button @click="destroyComponent">desmontar</button>
+    <a v-bind:href="product.url" v-bind:class="productClass">
+      {{product.name}}
+    </a>
+    <p>{{company}} é legal</p>
+    <p v-text="company"></p>  
+    <p>Acesse: <span v-html="link" /></p>  
+    <a href="#interno" v-on:click.prevent="something">link interno</a><br />
+    <a href="#interno" v-on:click.once="something">link interno</a><br />
+    <input type="text" @keyup="something" /><br />
+    <input type="text" @keyup.enter="something" /><br />
+    <input type="text" @keyup.k="something" /><br />
+    <p v-once>estoque inicial: {{stock}}</p>
+    <p>estoque atualizado: {{stock}}</p>
+    <button @click="stock++">aumentar estoque</button>
+    <div>
+      <h1 v-if="sex=== 'Masculino' ">masculino</h1>
+      <h1 v-else-if="sex === 'Feminino' ">feminino</h1>
+      <h1 v-else>outro</h1>
+    </div>
+    <h1 v-show="sex === 'Masculino' ">Masculino</h1>
 
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue' //tipagem typescript
+<script lang="ts"> 
+import { defineComponent } from 'vue' 
 
 export default defineComponent({
   data() {
     return {
-       count: 0
+      product: {
+        name: 'camisa',
+        url: 'https://loja.com/produtos/12345',
+        stock: true
+      },
+      products: [
+          { id: 1, name: 'Camisa', stock: true },
+          { id: 2, name: 'Bota', stock: false },
+          { id: 3, name: 'Chapéu', stock: false },
+          { id: 4, name: 'Cinto', stock: true }
+        ],
+      company: 'cataline',
+      link: '<a href="https://google.com.br">cataline</a>',
+      stock: 1,
+      sex: 'Feminino',
+
     }
-  }, 
-  beforeUnmount() {
-    console.log('salvar a contagem no db ')
   },
-  unmounted() {
-    alert('obrigado por usar!')
+  computed: {
+    productClass():string {
+      return this.product.stock ? 'success': 'danger'
+    }
   },
   methods: {
-    destroyComponent (){
-      this.$.appContext.app.unmount()
+    something(){
+      console.log('hwllow cataline')
     }
   }
 })
 </script>
+
+<style>
+  .danger{
+    color:red;
+  }
+  .success{
+    color: green;
+  }
+</style>
 
