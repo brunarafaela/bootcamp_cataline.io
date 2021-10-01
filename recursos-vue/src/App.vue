6f5a446665a85897d00e34ced9ab4fc272dd7bb1
brunarafaela <brunarafaelav@outlook.com>
2021-10-01 16:58:00 -0300

    Fim aula componentes

--- recursos-vue/src/App.vue
+++ recursos-vue/src/App.vue
@@ -1,36 +1,56 @@
 <template>
   <div>
-    <h1 class="static" :class="{active: isActive, 'text-danger': hasError}">
-    hwllow
-   </h1>
-    <h1 :class="['static', { 'text-danger': hasError}, className]">
-    hwllow
-  </h1>
+    <ul>
+      <li @click="currentView = 'Home' ">Home</li>
+      <li @click="currentView = 'About' " >Sobre</li>
+      <li @click="currentView = 'Contact' " >Contato</li>
+    </ul>
+
+    <component :is="currentView"/>
+
+    <ButtonStyled text="Facebook" color="primary" v-bind:user="user">
+      <template v-slot:before>
+        <i class="fab fa-facebook-f"></i>
+      </template>
+    </ButtonStyled>
+    <ButtonStyled text="Cadastrar" color="primary" @passarinho="action($event)"/>
+    <ButtonStyled text="Error" color="danger" />
+    <ButtonStyled text="Salvo!" color="success" />
+    <ButtonStyled v-bind:text="123" color="success" />
   </div>
-  
 </template>
 
 <script lang="ts"> 
 import { defineComponent } from 'vue' 
+import Home from '@/pages/Home.vue'
+import About from '@/pages/About.vue'
+import Contact from '@/pages/Contact.vue'
 
-export default defineComponent({
+export default defineComponent({ 
   data() {
     return { 
-      isActive: true,
-      hasError: false,
-      className: 'bruna-lima'
+      user: {
+      },
+      currentView: 'Home'
     }
   },
+  components :{
+    Home,
+    About,
+    Contact
+  },
   computed: {
-    
   },
   methods: { 
-  },
+    action(event: string){
+      console.log(event)
+    }
+  }, 
   directives: {
   }
 })
 </script>
 
-<style scoped> 
+<style> 
 </style> 
 
