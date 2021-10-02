a1cb9c04e833d06dd29f64df5d865ffef19ca08b
brunarafaela <brunarafaelav@outlook.com>
2021-10-02 00:56:40 -0300

    Fim modulo vuex

---
+++ gerenciamento-estado-vuex/src/store/index.js
@@ -0,0 +1,31 @@
+import { createStore } from 'vuex';
+
+export default createStore({
+    state: {
+        todos: [],
+    },
+    mutations: {
+        SET_TODOS(state, todos) {
+            state.todos = todos
+        },
+    },
+    actions: {
+        fetchTodos(context) {
+            const todos = [
+                { id: 1, text: 'Conceitos Vuex', done: true },
+                { id: 2, text: 'Atomic Design', done: false },
+                { id: 3, text: 'Começar com Nuxt', done: false },
+            ];
+
+            context.commit('SET_TODOS', todos)
+        },
+    },
+    getters: {
+        $allTodos(state) {
+            return state.todos
+        },
+        $doneTodos(state) {
+            return state.todos.filter(todo => todo.done)
+        },
+    },
+});
