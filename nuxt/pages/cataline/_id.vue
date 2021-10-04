5a73cd254a66177bac9c481b4e7a303ca00ed5d2
brunarafaela <brunarafaelav@outlook.com>
2021-10-04 01:39:45 -0300

    Fim aula nuxt plugins

---
+++ nuxt/pages/cataline/_id.vue
@@ -0,0 +1,19 @@
+<template>   
+  <div>
+    <h1> 
+      {{ $route.params }}
+    </h1>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    
+  })
+</script>
+
+<style scoped>
+
+</style>
---
+++ nuxt/pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Home</h1>
+    <ul>
+      <NuxtLink to="/">Home</NuxtLink>
+      <NuxtLink to="/sobre">Sobre</NuxtLink>
+    </ul>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue'
+
+  export default Vue.extend({
+    
+  })
+</script>
+
+<style>
+a.nuxt-link-exact-active {
+  font-weight: bold;
+}
+</style>
