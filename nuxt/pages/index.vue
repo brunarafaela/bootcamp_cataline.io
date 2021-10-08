7996c77e59dd56e7c091fecea80d31e2ee447188
brunarafaela <brunarafaelav@outlook.com>
2021-10-08 05:07:24 -0300

    Aula bosons, fix sass loader bug, inicio do projeto ibook

--- nuxt/pages/index.vue
+++ nuxt/pages/index.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1>Home</h1>
-    <ul>
-      <NuxtLink to="/">Home</NuxtLink>
-      <NuxtLink to="/sobre">Sobre</NuxtLink>
-    </ul>
-  </div>
+  <div class="box"></div>
 </template>
 
 <script lang="ts">
@@ -14,35 +8,36 @@
   export default Vue.extend({
     data() {
       return {
-        title: 'Teste de title'
       }
     },
-   head(this: any) {
-     return {
-        title: this.title,
-        htmlAttrs: {
-          lang: 'en'
-    },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' }
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
-   }
-   },
+  //  head(this: any) {
+  //    return {
+  //       title: this.title,
+  //       htmlAttrs: {
+  //         lang: 'en'
+  //   },
+  //   meta: [
+  //     { charset: 'utf-8' },
+  //     { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+  //     { hid: 'description', name: 'description', content: '' },
+  //     { name: 'format-detection', content: 'telephone=no' }
+  //   ],
+  //   link: [
+  //     { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+  //   ]
+  //  }
+  //  },
     mounted() {
-      const posts = this.$axios.$get('/posts')
     }
     
   })
 </script>
 
-<style>
-a.nuxt-link-exact-active {
-  font-weight: bold;
+<style lang="scss" scoped>
+.box {
+width: 300px;
+height: 300px;
+/* background-color: color('success', 'darkest'); */
+background-color: #000;
 }
 </style>
